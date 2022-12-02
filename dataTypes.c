57b74eb80f69176d052a8b6eaab5bd7dc8be97c2
himchan94 <himchan9494@gmail.com>
2022-12-02 23:38:42 +0900

    feat: Create basic types

---
+++ dataTypes.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int age = 40;
+  double gpa = 40.0;
+  char grade = 'A';  // one single character
+  char phrase[] = "ABCDEFG";
+
+  return 0;
+}
