c14f0c454ea5bf4fd38329e121ece95e6027ff5b
himchan94 <himchan9494@gmail.com>
2022-12-03 23:59:47 +0900

    feat: constants

---
+++ constants.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+/*
+  int num = 5;
+  printf("%d\n", num);
+  num = 8;
+  printf("%d\n", num);
+*/
+  const int FAV_NUM = 5;  // declare constants
+  printf("%d\n", FAV_NUM);
+
+  // FAV_NUM = 8; Error
+  printf("%d\n", FAV_NUM);
+  return 0;
+}
