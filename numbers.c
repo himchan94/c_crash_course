bbdef07f49af2bd7c3d620799e6d37887cf1303b
himchan94 <himchan9494@gmail.com>
2022-12-03 23:59:53 +0900

    feat: numbers

---
+++ numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+
+  printf("%f\n", pow(2, 3));
+  printf("%f\n", sqrt(36));
+  printf("%f\n", ceil(36.356));
+  printf("%f\n", floor(36.356));
+
+  return 0;
+}
